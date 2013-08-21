d8e254f454ee2e7a04438c8536ce3934e901b051
that0n3guy <that0n3guy@46440.no-reply.drupal.org>
2013-08-21 11:15:02 -0500

    smb login info write to root

--- smb_login_mount.sh
+++ smb_login_mount.sh
@@ -12,20 +12,20 @@ read smbmount
 
 echo
 echo "Creating .smb_credentials: "
-echo sudo "username=$smbuser" > ~/.smb_credentials
-echo sudo "password=$smbpass" >> ~/.smb_credentials
+echo sudo "username=$smbuser" > /root/.smb_credentials
+echo sudo "password=$smbpass" >> /root/.smb_credentials
 echo
 echo
 
 echo "Creating .smb_remote_shares: "
-echo sudo "[$smbmount]" > ~/.smb_remote_shares
+echo sudo "[$smbmount]" > /root/.smb_remote_shares
 echo
 
 
 echo "Installing cifs-utils"
 sudo apt-get install cifs-utils -y
 
-echo "Check ~/.smb_credentials to verify your samba login info is correct"
-echo "Check ~/.smb_remote_shares to verify your samba mounts are correct and add additional mounts"
+echo "Check /root/.smb_credentials to verify your samba login info is correct"
+echo "Check /root/.smb_remote_shares to verify your samba mounts are correct and add additional mounts"
 
 
